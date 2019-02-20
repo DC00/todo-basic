c6cc8ef9b9943366a2606cd3f14e8730eec514f0
DC00 <djc4ku@virginia.edu>
2019-02-20 14:03:34 -0500

    comments

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.engine('html', ejs.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// adding, viewing, and deleting todos
 var todos = {
-  "work out" : false,
-  "wake up" : false
+  "wake up" : false,
+  "work out" : false
 }
 
+// View all todos on index page
 app.get('/', (req, res) => {
   var incomplete_todos = []
   Object.keys(todos).forEach(k => {
@@ -26,12 +26,14 @@ app.get('/', (req, res) => {
   res.render("index", { "todos": incomplete_todos });
 });
 
+// Create a todo if non-empty name
 app.post('/add', (req, res) => {
   console.log(req.body.name);
   if (req.body.name) todos[req.body.name] = false;
   res.status(200).redirect('/')
 });
 
+// Complete a todo, stored as true after
 app.post('/done', (req, res) => {
   if (req.body.name in todos) todos[req.body.name] = true
   res.status(200).redirect('/')
